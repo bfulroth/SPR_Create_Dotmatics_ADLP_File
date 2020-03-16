0b001e41d4ff3d7ea3cca50005676e45d673fb28
Ben Fulroth <fulrothb@icloud.com>
2020-03-16 16:44:54 -0400

    Renamed packages to be consistent with PEP 8

--- SCRIPT_SPR_SETUP_FILE/__main__.py
+++
@@ -1,4 +0,0 @@
-from SCRIPT_SPR_SETUP_FILE.Create_SPR_setup_file import spr_setup_sheet
-
-if __name__ == '__main__':
-    spr_setup_sheet()
--- SCRIPT_SPR_TO_ADLP_8K/__main__.py
+++
@@ -1,4 +0,0 @@
-from SCRIPT_SPR_TO_ADLP_8K.SPR_to_ADLP_8K import spr_create_dot_upload_file
-
-if __name__ == '__main__':
-    spr_create_dot_upload_file()
