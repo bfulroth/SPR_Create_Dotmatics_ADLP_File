435ab721ab092b0bbb2384cc594f644a7b96ebcd
Ben Fulroth <fulrothb@icloud.com>
2020-04-10 17:32:46 -0400

    Clean up: Refactored Temp dir creation and struct img methods

--- script_spr_to_adlp_not_8k/SPR_to_ADLP.py
+++ script_spr_to_adlp_not_8k/SPR_to_ADLP.py
@@ -16,16 +16,19 @@ else:
     homedir = os.environ['HOME']
 
 
-STRUCT_IMG_DIR = os.path.join(os.getcwd(), 'structure_images')
-
-
-def get_structures_from_db(df_mstr_tbl):
+def get_structures_smiles_from_db(df_mstr_tbl):
+    """
+    This method creates a connection to results db and retrieves the smiles for each BRD in the passed in df_mstr_tbl
+    :return DataFrame containing BRD, CORE ID, and SMILES
+    """
 
     # Extract the CORE Broad ID from the df_mstr_tbl
     df_brd_core_id = df_mstr_tbl[['Broad ID']]
     df_brd_core_id.loc[:, 'BROAD_CORE_ID'] = df_brd_core_id['Broad ID'].apply(lambda x: x[5:13])
-    sort_vals = [i for i in range(0, len(df_brd_core_id))]
-    df_brd_core_id.loc[:, 'sort_col'] = pd.Series(sort_vals)
+
+    # TODO: Make sure sorting the final df is not need at the end.
+    # sort_vals = [i for i in range(0, len(df_brd_core_id))]
+    # df_brd_core_id.loc[:, 'sort_col'] = pd.Series(sort_vals)
 
     # Connect to resultsdb database.
     try:
@@ -52,7 +55,7 @@ def get_structures_from_db(df_mstr_tbl):
         conn = engine.connect()
     except Exception:
         raise ConnectionError("Cannot connect to resultsdb database. "
-                              "Compound structures will not be rendered in final file.")
+                              "Please make sure you are on the Broad Internal Network.")
 
     # Reflect Tables
     metadata = MetaData()
@@ -74,14 +77,23 @@ def get_structures_from_db(df_mstr_tbl):
     # RENAME the column headers.
     df_struct_tbl.columns = ['BROAD_CORE_ID', 'SMILES']
 
-    # Make a new directory to store rendered structure images
-    os.mkdir(STRUCT_IMG_DIR)
-
     # Merge df_struct_tbl with df_brd_core_id such that Full BRD, CORE ID, and SMILES are in the same table
     df_merge_full_brd_smiles = pd.merge(left=df_brd_core_id, right=df_struct_tbl, on='BROAD_CORE_ID', how='left')
 
+    return df_merge_full_brd_smiles
+
+
+def render_structure_imgs(df_with_smiles, dir):
+    """
+    Does the work of rendering images from smiles using RDkit into a directory
+
+    :param df_with_smiles: DataFrame contain the smiles to render
+    :param dir: directory to story the images.
+    :return none
+    """
+
     # Create an image Path Column
-    df_merge_full_brd_smiles.loc[:, 'IMG_PATH'] = ''
+    df_with_smiles.loc[:, 'IMG_PATH'] = ''
 
     # Use BRD control as a template to align molecules.
     template = Chem.MolFromSmiles('Nc1c(F)cc(C#N)c2cc[nH]c12')
@@ -91,11 +103,11 @@ def get_structures_from_db(df_mstr_tbl):
 
     # Create the images from smiles in a new directory
     # TODO: Attempt to align structure doesn't fully work
-    for idx, row in df_merge_full_brd_smiles.iterrows():
+    for idx, row in df_with_smiles.iterrows():
 
         current_smile_str = row['SMILES']
         img_name = str(img_num) + '_' + row['Broad ID'] + '.png'
-        img_full_path = os.path.join(STRUCT_IMG_DIR, img_name)
+        img_full_path = os.path.join(dir, img_name)
 
         # Generate the structure of the current molecule using the control as a template to align
         m = Chem.MolFromSmiles(current_smile_str)
@@ -103,11 +115,11 @@ def get_structures_from_db(df_mstr_tbl):
         Draw.MolToFile(mol=m, filename=img_full_path, size=(300, 300))
 
         # Save the image path to the DataFrame
-        df_merge_full_brd_smiles.loc[idx, 'IMG_PATH'] = img_full_path
+        df_with_smiles.loc[idx, 'IMG_PATH'] = img_full_path
 
         img_num += 1
 
-    return df_merge_full_brd_smiles
+    return df_with_smiles
 
 
 def dup_item_for_dot_df(df, col_name, times_dup=3, sort=False):
@@ -139,7 +151,7 @@ def dup_item_for_dot_df(df, col_name, times_dup=3, sort=False):
         print("The DataFrame does not have a " + col_name + " column.")
 
 
-def spr_insert_images(tuple_list_imgs, worksheet, path_ss_img, path_senso_img):
+def spr_insert_ss_senso_images(tuple_list_imgs, worksheet, path_ss_img, path_senso_img):
     """
     Does the work of inserting the spr steady state and sensorgram images into the excel worksheet.
     :param tuple_list: List of tuples containing (steady state image, sensorgram image)
@@ -164,11 +176,11 @@ def spr_insert_images(tuple_list_imgs, worksheet, path_ss_img, path_senso_img):
         worksheet.insert_image('F' + str(row), path_senso_img + '/' + senso_img)
         row += 1
 
-# TODO: Clean up this method and docs
+
 def spr_insert_structures(ls_img_struct_paths, worksheet):
     """
-    Does the work of inserting the spr steady state and sensorgram images into the excel worksheet.
-    :param tuple_list: List of tuples containing (steady state image, sensorgram image)
+    Does the work of inserting the structures into the xlsxwriter workbook object.
+    :param ls_img_struct_paths: list of image paths to insert.
     :param worksheet: xlsxwriter object used to insert the images to a worksheet
     :return: None
     """
@@ -180,7 +192,7 @@ def spr_insert_structures(ls_img_struct_paths, worksheet):
         worksheet.set_row(row=row, height=235)
 
     # Set the width of each column
-    worksheet.set_column(first_col=1, last_col=1, width=40)
+    worksheet.set_column(first_col=1, last_col=1, width=45)
 
     row = 2
     for img in ls_img_struct_paths:
@@ -620,27 +632,30 @@ def spr_create_dot_upload_file(config_file, save_file, clip):
     # Create a list of tuples containing the names of the steady state image and sensorgram image.
     tuple_list_imgs = list(zip(list_ss_img, list_sonso_img))
 
-    # Insert images into file.
-    spr_insert_images(tuple_list_imgs, worksheet1, path_ss_img, path_senso_img)
+    # Insert steady-state and sensogram images into file.
+    spr_insert_ss_senso_images(tuple_list_imgs, worksheet1, path_ss_img, path_senso_img)
 
-    # Start preparing to insert structure images
-    # This line gets all the smiles from the database and creates the images as png files in a directory
-    df_struct_imgs = get_structures_from_db(df_mstr_tbl=df_cmpd_set)
+    # Insert structure images
+    # Render the smiles into png images in a temp directory
+    with tempfile.TemporaryDirectory() as tmp_img_dir:
 
-    # Create an list of the images paths in order
-    ls_img_paths = dup_item_for_dot_df(df=df_struct_imgs, col_name='IMG_PATH', times_dup=num_fc_used)
+        # This line gets all the smiles from the database
+        df_struct_smiles = get_structures_smiles_from_db(df_mstr_tbl=df_cmpd_set)
 
-    # Insert the structures into the Excel workbook object
-    spr_insert_structures(ls_img_struct_paths=ls_img_paths, worksheet=worksheet1)
+        # Render the structure images
+        df_with_paths = render_structure_imgs(df_with_smiles=df_struct_smiles, dir=tmp_img_dir)
 
-    # TODO: Remove the structure directory when done
-    #os.rmdir(STRUCT_IMG_DIR)
+        # Create an list of the images paths in order
+        ls_img_paths = dup_item_for_dot_df(df=df_with_paths, col_name='IMG_PATH', times_dup=num_fc_used)
 
-    # Close the Pandas Excel writer and output the Excel file.
-    writer.save()
+        # Insert the structures into the Excel workbook object
+        spr_insert_structures(ls_img_struct_paths=ls_img_paths, worksheet=worksheet1)
+
+        # Save the writer object inside the context manager.
+        writer.save()
+
     print('Program Done!')
     print("The ADLP result was saved to your desktop.")
-
 
 if __name__ == '__main__':
     spr_create_dot_upload_file()
