f0c16ae9d160f4d9cf8c0472cdd135c0ba1c106e
Ben Fulroth <fulrothb@icloud.com>
2020-05-15 09:51:26 -0400

    Testing: Start implementing Testing Module for SPR_to_ADLP.py

---
+++ tests/test_spr_to_adlp_not_8k.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from unittest.mock import Mock, MagicMock, patch
+import os
+from script_spr_to_adlp_not_8k.SPR_to_ADLP import *
+import pandas as pd
+
+
+class SPR_to_ADLP_not_8k(TestCase):
+
+    pass
+
+
+
+
