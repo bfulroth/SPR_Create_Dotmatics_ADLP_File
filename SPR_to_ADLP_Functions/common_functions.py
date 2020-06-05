30c7c75392d2b08a33c4ca38b64e2614983f8220
Ben Fulroth <fulrothb@icloud.com>
2020-06-05 09:34:17 -0400

    Bug Fix: Crash if smile not found in db

--- SPR_to_ADLP_Functions/common_functions.py
+++ SPR_to_ADLP_Functions/common_functions.py
@@ -1,6 +1,7 @@
 """Module that contains common functions for SPR to ADLP aggregation scripts"""
 import os
 import pandas as pd
+import numpy as np
 from rdkit import Chem
 from rdkit.Chem import Draw, AllChem
 import cx_Oracle
@@ -38,9 +39,11 @@ def rep_item_for_dot_df(df, col_name, times_dup=3, sort=False):
     except:
         raise RuntimeError("The DataFrame does not have a " + col_name + " column.")
 
+
 def _connect(engine):
     c = engine.connect()
     return c
+
 
 def get_structures_smiles_from_db(df_mstr_tbl):
     """
@@ -111,7 +114,6 @@ def get_structures_smiles_from_db(df_mstr_tbl):
 
     # Merge df_struct_tbl with df_brd_core_id such that Full BRD, CORE ID, and SMILES are in the same table
     df_merge_full_brd_smiles = pd.merge(left=df_brd_core_id, right=df_struct_tbl, on='BROAD_CORE_ID', how='left')
-
     return df_merge_full_brd_smiles
 
 
@@ -123,6 +125,9 @@ def render_structure_imgs(df_with_smiles, dir):
     :param dir: directory to story the images.
     :return none
     """
+
+    # Tag the smile field if it is not found.
+    df_with_smiles = df_with_smiles.fillna('Not Found')
 
     # Create an image Path Column
     df_with_smiles.loc[:, 'IMG_PATH'] = ''
@@ -138,18 +143,25 @@ def render_structure_imgs(df_with_smiles, dir):
     for idx, row in df_with_smiles.iterrows():
 
         current_smile_str = row['SMILES']
-        img_name = str(img_num) + '_' + row['Broad ID'] + '.png'
-        img_full_path = os.path.join(dir, img_name)
 
-        # Generate the structure of the current molecule using the control as a template to align
-        m = Chem.MolFromSmiles(current_smile_str)
-        #AllChem.GenerateDepictionMatching2DStructure(m, template)
-        Draw.MolToFile(mol=m, filename=img_full_path, size=(300, 300))
+        if current_smile_str == 'Not Found':
+            df_with_smiles.loc[idx, 'IMG_PATH'] = 'Not Found'
+            img_num += 1
 
-        # Save the image path to the DataFrame
-        df_with_smiles.loc[idx, 'IMG_PATH'] = img_full_path
+        else:
+            img_name = str(img_num) + '_' + row['Broad ID'] + '.png'
+            img_full_path = os.path.join(dir, img_name)
+            print(img_full_path)
 
-        img_num += 1
+            # Generate the structure of the current molecule using the control as a template to align
+            m = Chem.MolFromSmiles(current_smile_str)
+            #AllChem.GenerateDepictionMatching2DStructure(m, template)
+            Draw.MolToFile(mol=m, filename=img_full_path, size=(300, 300))
+
+            # Save the image path to the DataFrame
+            df_with_smiles.loc[idx, 'IMG_PATH'] = img_full_path
+
+            img_num += 1
 
     return df_with_smiles
 
@@ -174,8 +186,13 @@ def spr_insert_structures(ls_img_struct_paths, worksheet):
 
     row = 2
     for img in ls_img_struct_paths:
-        worksheet.insert_image('B' + str(row), img)
-        row += 1
+
+        if img == 'Not Found':
+            row += 1
+
+        else:
+            worksheet.insert_image('B' + str(row), img)
+            row += 1
 
 
 def spr_insert_ss_senso_images(tuple_list_imgs, worksheet, path_ss_img, path_senso_img, biacore):
